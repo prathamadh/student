31ab24c84ea737ca160693a1987f4af66d75e80c
prathamadh <apratham489@gmail.com>
2025-06-06 14:40:37 +0545

    cleared output confidence form basemodel.py

--- training/mono/model/__base_model__.py
+++ training/mono/model/__base_model__.py
@@ -31,7 +31,7 @@ class BaseDepthModel(nn.Module):
         losses_dict = {}
         if self.training:
             output.update(data)
-            # losses_dict = self.get_loss(output)
+            losses_dict = self.get_loss(output)
             losses_dict={}
         if self.downsample != None:
             self.pred_upsample(self.downsample, output)
