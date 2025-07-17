fixed unnecessary data into model and loss calculation

training/mono/model/__base_model__.py
@@ -151,10 +151,9 @@ class BaseDepthModel(nn.Module):
         args = dict(pred_logit=pred_logit)
         
         target = kwargs.pop('target')
-        args.update(kwargs)
-
         # data type for each batch
-        batches_data_type = np.array(kwargs['data_type']) 
+        batches_data_type = np.array(kwargs.pop('data_type'))
+        args.update(kwargs) 
         # batches_data_names = np.array(kwargs['dataset']) 
 
         # resize the target

training/mono/model/decode_heads/RAFTDepthNormalDPTDecoder5.py
@@ -984,16 +984,17 @@ class RAFTDepthNormalDPT5(nn.Module):
         
         outputs=dict(
             prediction=flow_predictions[-1],
-            # predictions_list=flow_predictions,
+            predictions_list=flow_predictions,
             # confidence=conf_predictions[-1],
             # confidence_list=conf_predictions,
             pred_logit=None,
             # samples_pred_list=samples_pred_list,
             # coord_list=coord_list,
             # prediction_normal=normal_outs[-1],
-            # normal_out_list=normal_outs,
+            normal_out_list=normal_outs,
             # low_resolution_init=low_resolution_init,
             rm=rm_outs,
+        
         )
 
         return outputs